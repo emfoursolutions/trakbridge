210ed90313960421e819cff20e2c9c7550f99457
Emfour Developer <nick@emfour.net>
2025-08-04 11:03:01 +0400

    Fixed UID and GID detection and export

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -309,6 +309,10 @@ deploy_services() {
     export APP_VERSION="$env-latest"
     export COMPOSE_FILE="$COMPOSE_FILE"
     
+    # Export UID/GID for dynamic user creation
+    export UID=${USER_ID:-$(id -u)}
+    export GID=${GROUP_ID:-$(id -g)}
+    
     # Set image tag for pre-built images
     if [[ "$USE_PREBUILT" == "true" ]]; then
         # For feature branches, use the branch tag
